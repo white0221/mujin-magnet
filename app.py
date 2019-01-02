74bb8ce4e1359a4c553b1a72b792aa8bb5c78c26
AltTether <200351p@ugs.kochi-tech.ac.jp>
2019-01-03 01:43:26 +0900

    レスポンスをjson化

--- app.py
+++ app.py
@@ -1,14 +1,17 @@
-from flask import Flask
+from flask import Flask, jsonify, make_response
 from src import Magnet
 
+
 app = Flask(__name__)
-
-
 mag = Magnet()
 
 @app.route('/')
 def main():
-    return str(mag.get_state())
+    response = {'state': None, 'error': None}
+    response['state'] = mag.get_state()
+    response = make_response(jsonify(response))
+    response.mimetype = 'application/json'
+    return response
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
