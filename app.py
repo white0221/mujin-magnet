a7cf43fc185436b46290600b577b6f13237bacb5
筒井まや乃 <tsutsui@MacBook-Pro-9.local>
2019-01-22 00:33:35 +0900

    ストリーミング作成、エラー処理削除

--- app.py
+++ app.py
@@ -1,17 +1,16 @@
-from flask import Flask, jsonify, make_response
+from flask import Flask, json, make_response, stream_with_context, request, Response
 from src import Magnet
-
 
 app = Flask(__name__)
 mag = Magnet()
 
 @app.route('/')
 def main():
-    response = {'state': None, 'error': None}
-    response['state'] = mag.get_state()
-    response = make_response(jsonify(response))
-    response.mimetype = 'application/json'
-    return response
+  def generate(): 
+     while(True):
+         yield '{"state":'
+         yield json.dumps(mag.get_state()) + '}'
+  return Response(stream_with_context(generate()),mimetype = 'application/json')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
